c206c51a1d171521d3bace3cfb39e14538464bbb
P-PC\SJ <i.am.blooooooo@gmail.com>
2017-09-01 01:53:26 +0900

    난 알고리즘도 못짜는 병신

--- pyramid/pyramid.c
+++ pyramid/pyramid.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
+int pyramid()
+{
+	int a;
+	int i;
+	for (i = 1; i < 3; i++)
+	{
+		// 2n+1
+		if (a<i)
+			printf("_");
+		printf("¤±");
+	}
+}
+
 int main(void)
 {
 	int a;
-	printf("ses");
+
+	printf("¿øÇÏ´Â Ãþ : ");
+	scanf("%d", &a);
+	pyramid();
 
 	return 0;
 }
