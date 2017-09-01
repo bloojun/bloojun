20e6e39cd5798dc9dd2461e4f712301562943f27
P-PC\SJ <i.am.blooooooo@gmail.com>
2017-09-02 03:26:41 +0900

    나는 스택도 못짜는 병신

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+#define MAX 10
+
+typedef struct stack {
+	int arr[MAX];
+	int top;
+}STACK;
+
+void push()
+{
+	
+}
+
+void pop()
+{
+
+}
+
 int main(void)
 {
+	STACK s;
+
+	s.top = -1;
+
+	push();
+
 	return 0;
 }
