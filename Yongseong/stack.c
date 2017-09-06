07879e58d50276b2844ab0c8efb02e80e2373350
MOONYS\MoonYS <forever9882@gmail.com>
2017-09-06 11:07:27 +0900

    추가수정 17.09.06 11:07

--- Yongseong/stack.c
+++ Yongseong/stack.c
@@ -1,6 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#define Bufsize 4096
+
+#define TRUE 0;
+#define FALSE -1;
 
 typedef struct ArrayStack
 {
@@ -20,38 +24,41 @@ void CreateStack(ArrayStack **Stack,int Capacity)
 	(*Stack)->Top = 0;
 }
 //데이터와 라인번호 Push
-void Push(ArrayStack *Stack,char Data, int Linecount)
+int Push(ArrayStack *Stack,char Data, int Linecount)
 {
+	if (!Stack || Stack->Capacity == Stack->Top)
+	{
+		printf("Stack is full. \n");
+		return FALSE;
+	}
+	
 	Stack->Data[Stack->Top] = Data;
 	Stack->LineNum[Stack->Top] = Linecount;
 	(Stack->Top)++;
 	
 	//printf("Push: %c \n", Data);
-
-	if (Stack->Capacity == Stack->Top)
-	{
-		printf("스택이 가득 찼습니다. \n");
-	}
+	return TRUE;
 }
 
 //POP
-void Pop(ArrayStack* Stack)
+int Pop(ArrayStack* Stack)
 {
-	if (Stack->Top == 0)
+	if (!Stack || Stack->Top == 0)
 	{
-		printf("스택이 비었습니다. \n");
+		printf("Stack is empty. \n");
+		return FALSE;
 	}
-	else
-	{
+	
 		 --(Stack->Top);
-	}
+	
 	//printf("Pop: %c \n", Stack->Data);
+		 return TRUE;
 }
 //괄호 오류체크 함수
 int CheckBracket(ArrayStack *Stack, char * buf,int Linecount)
 {
 	int idx;
-	for (idx = 0; idx < 100; idx++) {
+	for (idx = 0; idx < Bufsize; idx++) {
 		switch (buf[idx])
 		{
 		case '{':
@@ -70,22 +77,21 @@ int CheckBracket(ArrayStack *Stack, char * buf,int Linecount)
 			{
 				printf("%d 라인에서 %c의 짝이 맞지 않습니다.\n", Stack->LineNum[Stack->Top-1], Stack->Data[Stack->Top-1]);
 				//getch(); 
-				exit(-1);
+				return FALSE;
 			}
 			break;
 		default:
 			break;
 		}
 	}
-	return 0;
+	return TRUE;
 }
 
 int main()
 {
 	//줄의 번호 카운팅
 	int linecount = 0;
-
-	char buf[100] = {0};
+	
 	ArrayStack* Stack = NULL;
 
 	CreateStack(&Stack, 100);
@@ -94,25 +100,37 @@ int main()
 	if (fp == NULL)
 	{
 		printf("파일 열기 실패");
-		return -1;
+		return FALSE;
 	}
+
+	//4KByte
+	char buf[Bufsize] = { 0 };
 
 	do
 	{
 		memset(buf, 0, sizeof(buf));
-		if (fgets(buf, sizeof(buf), fp) == 0)
+		if (fgets(buf, sizeof(buf), fp)==0)
+		{
 			break;
+		}
+		
 		linecount++;
 		printf("%d: %s", linecount, buf);
 		//괄호체크
-		CheckBracket(Stack, buf, linecount);
+		if (CheckBracket(Stack, buf, linecount) != 0)
+		{
+			break;
+		}
+			
 	} while(!feof(fp));
 
 	fclose(fp);
 	//메모리 해제
-	free(Stack->Data);
-	free(Stack->LineNum);
-	free(Stack);
-
-	return 0;
+	if (Stack != 0) 
+	{
+		free(Stack->Data);
+		free(Stack->LineNum);
+		free(Stack);
+	}
+	return TRUE;
 }
